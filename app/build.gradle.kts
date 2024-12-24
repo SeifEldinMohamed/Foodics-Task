a87fdc45a3fb884fbf6824f31cd6008d476ce5b5
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-12-24 08:17:20 +0200

    setup project and add needed dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.example.foodicstask"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.foodicstask"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // lottie compose
+    implementation(libs.lottie.compose)
+    // coil compose
+    implementation(libs.coil.compose)
+    // navigation  compose
+    implementation(libs.androidx.navigation.compose)
+
+    // mockk
+    testImplementation(libs.mockk)
+    // turbine
+    testImplementation(libs.turbine)
+
+    // lifecycle
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.lifecycle.viewmodel.ktx)
+
+    // coroutines
+    implementation(libs.coroutines)
+    testImplementation(libs.coroutines.test)
+
+    // room
+    implementation(libs.room)
+    ksp(libs.room.compiler)
+
+    // koin
+    implementation(libs.koin.android)
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.koin.core)
+    implementation(libs.koin.compose)
+    implementation(libs.koin.coroutines)
+    implementation(libs.koin.compose.viewmodel)
+    // Added
+    implementation(libs.koin.annotation)
+    ksp(libs.koin.ksp.compiler)
+}
