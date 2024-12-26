516d263fed5e02e43472f5b91ceb17981d5e1c2d
Seif <seifeldinh@banquemisr.com>
2024-12-26 07:22:46 +0200

    update Colors, Typography, shapes strings and theme

---
+++ app/src/main/java/com/example/foodicstask/presentation/theme/Color.kt
@@ -0,0 +1,52 @@
+package com.example.foodicstask.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val light_primary = Color(0xFFFFFFFF)
+val light_onPrimary = Color(0xFF000000)
+
+val light_secondary = Color(0xFF0422CE)
+val light_onSecondary = Color(0xFFFFFFFF)
+
+val light_tertiary = Color(0xFF24893d)
+val light_onTertiary = Color(0xFFFFFFFF)
+
+val light_error = Color(0xFFBA1A1A)
+val light_onError = Color(0xFFFFFFFF)
+
+val light_background = Color(0xFFFFFFFF)
+val light_onBackground = Color(0xFF191C1C)
+
+val light_surface = Color(0xFFFFFFFF)
+val light_onSurface = Color(0xFF000000)
+
+val light_surface_variant = Color(0xFFEEEEF1)
+val light_onSurface_variant = Color(0xFF888888)
+
+val LightGray = Color(0xFF929292)
+val Gray = Color(0xFFe5e5ea)
+val LightGreen = Color(0xFF31B057)
+val Red = Color(0xFFFF0000)
+val DarkRed = Color(0xFFC50000)
+val LightRed = Color(0xFFFE0A58)
+
+val dark_primary = Color(0xFF212121)
+val dark_onPrimary = Color(0xFFF7F7F7)
+
+val dark_secondary = Color(0xFF011277)
+val dark_onSecondary = Color(0xFFF7F7F7)
+
+val dark_tertiary = Color(0xFF148931)
+val dark_onTertiary = Color(0xFFF7F7F7)
+
+val dark_error = Color(0xFF810808)
+val dark_onError = Color(0xFFF7F7F7)
+
+val dark_background = Color(0xFF212121)
+val dark_onBackground = Color(0xFFF7F7F7)
+
+val dark_surface = Color(0xFF1B1B1B)
+val dark_onSurface = Color(0xFFFFFFFF)
+
+val dark_surface_variant = Color(0xFF272727)
+val dark_onSurface_variant = Color(0xFFEBEBEB)
