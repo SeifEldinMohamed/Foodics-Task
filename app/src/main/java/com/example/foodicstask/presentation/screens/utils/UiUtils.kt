cf733a1b0d902b2dfa2e300959ab6bfb2c5b98dd
Seif <seifeldinh@banquemisr.com>
2024-12-26 08:19:59 +0200

    create ViewOrderButton, NavigationMenuItem.kt and UiUtils.kt

---
+++ app/src/main/java/com/example/foodicstask/presentation/screens/utils/UiUtils.kt
@@ -0,0 +1,25 @@
+package com.example.foodicstask.presentation.screens.utils
+
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+fun WindowSizeClass.getGridCellsCount(): Int {
+    val gridCellsCountInPhones = 2
+    val gridCellsCountInTablets = 4
+
+    return when (this.widthSizeClass == WindowWidthSizeClass.Compact) {
+        true -> gridCellsCountInPhones
+        false -> gridCellsCountInTablets
+    }
+}
+
+@Composable
+fun WindowSizeClass.getTopPadding(): Dp {
+    return when (this.widthSizeClass == WindowWidthSizeClass.Compact) {
+        true -> 16.dp
+        false -> 0.dp
+    }
+}
