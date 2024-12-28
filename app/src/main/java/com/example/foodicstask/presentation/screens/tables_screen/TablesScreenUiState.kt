ee1eb8de5cc4b9aa9bef577ba733c022dd2c439d
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-12-28 21:41:56 +0200

    adjust TablesScreenUiState and CategoriesBar.kt

--- app/src/main/java/com/example/foodicstask/presentation/screens/tables_screen/TablesScreenUiState.kt
+++ app/src/main/java/com/example/foodicstask/presentation/screens/tables_screen/TablesScreenUiState.kt
@@ -8,8 +8,10 @@ import com.example.foodicstask.presentation.screens.tables_screen.model.Category
 
 sealed class TablesScreenUiState{
     data object EmptyState: TablesScreenUiState()
-    data class Loading(val isLoading:Boolean): TablesScreenUiState()
+    data class LoadingScreen(val isLoading:Boolean): TablesScreenUiState()
     data class FetchedTableData(val foodList: List<FoodItemUiModel>, val categoryList: List<CategoryUiModel>): TablesScreenUiState()
+    data class LoadingFoodList(val isLoading:Boolean, val categoriesList:List<CategoryUiModel>): TablesScreenUiState()
+    data class FilteredFoodsByCategory(val filteredFoodList: List<FoodItemUiModel>, val categoriesList:List<CategoryUiModel>): TablesScreenUiState()
     data class ApiError(val customApiErrorExceptionUiModel: CustomApiExceptionUiModel): TablesScreenUiState()
     data class DatabaseError(val customDatabaseExceptionUiModel: CustomDatabaseExceptionUiModel): TablesScreenUiState()
 }
