804759e7e4a1aa9944b93e9abfed29c128616db9
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-12-28 15:02:14 +0200

    add FoodItemEntity and CategoryEntity

---
+++ app/src/main/java/com/example/foodicstask/data/data_sources/local/room/entities/FoodItemEntity.kt
@@ -0,0 +1,14 @@
+package com.example.foodicstask.data.data_sources.local.room.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "food_items")
+data class FoodItemEntity(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val description: String,
+    val image: String,
+    val price: Float,
+    val category: CategoryEntity
+)
