d8d1cf2abb9e3d799833bd5d51de24d42970b980
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-12-29 21:06:30 +0200

    update mappers

--- app/src/main/java/com/example/foodicstask/data/mapper/food/ToFoodItemEntity.kt
+++ app/src/main/java/com/example/foodicstask/data/mapper/food/ToFoodItemEntity.kt
@@ -1,7 +1,7 @@
 package com.example.foodicstask.data.mapper.food
 
 import com.example.foodicstask.data.data_sources.local.room.entities.FoodItemEntity
-import com.example.foodicstask.data.data_sources.remote.ktor.model.food_list.FoodItemDataModel
+import com.example.foodicstask.data.data_sources.remote.ktor.model.tables.FoodItemDataModel
 
 fun FoodItemDataModel.toFoodItemEntity(): FoodItemEntity {
     return FoodItemEntity(
