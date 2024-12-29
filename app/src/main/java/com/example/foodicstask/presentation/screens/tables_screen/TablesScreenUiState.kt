3b5c0e0dc1b3d7e7e30232d4e8446efbafee039c
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2024-12-29 21:11:36 +0200

    finish implementation of add products to cart

--- app/src/main/java/com/example/foodicstask/presentation/screens/tables_screen/TablesScreenUiState.kt
+++ app/src/main/java/com/example/foodicstask/presentation/screens/tables_screen/TablesScreenUiState.kt
@@ -7,12 +7,14 @@ import com.example.foodicstask.presentation.screens.tables_screen.model.Category
 
 
 sealed class TablesScreenUiState{
-    data object EmptySearchState: TablesScreenUiState()
+    data object EmptyState: TablesScreenUiState()
     data class LoadingScreen(val isLoading:Boolean): TablesScreenUiState()
     data class FetchedTableData(val foodList: List<FoodItemUiModel>, val categoryList: List<CategoryUiModel>): TablesScreenUiState()
+
     data class LoadingFoodList(val isLoading:Boolean, val categoriesList:List<CategoryUiModel>): TablesScreenUiState()
     data class FilteredFoodsByCategory(val filteredFoodList: List<FoodItemUiModel>, val categoriesList:List<CategoryUiModel>): TablesScreenUiState()
-        data class SearchedFoodsByName(val searchedFoodList: List<FoodItemUiModel>, val categoriesList:List<CategoryUiModel>): TablesScreenUiState()
+    data class SearchedFoodsByName(val searchedFoodList: List<FoodItemUiModel>, val categoriesList:List<CategoryUiModel>): TablesScreenUiState()
+
     data class ApiError(val customApiErrorExceptionUiModel: CustomApiExceptionUiModel): TablesScreenUiState()
     data class DatabaseError(val customDatabaseExceptionUiModel: CustomDatabaseExceptionUiModel): TablesScreenUiState()
 }
